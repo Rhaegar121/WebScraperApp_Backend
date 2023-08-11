c8e4cfadbccec15443aea22a549d0905e12c2b0f
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-08-11 15:57:58 +0630

    add necessary validations in models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
     belongs_to :category
     has_many :product_images
+
+    validates :url, presence: true, uniqueness: true
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :price, presence: true
 end
