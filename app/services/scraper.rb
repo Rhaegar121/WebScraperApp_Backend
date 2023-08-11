52468de0d9abae81812d3690f8dafac8b9383268
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-08-11 22:38:06 +0630

    save the scraped data in the database

--- app/services/scraper.rb
+++ app/services/scraper.rb
@@ -1,26 +1,39 @@
 require 'nokogiri'
 require 'open-uri'
+require_relative '../models/product'
 
 class Scraper
-  def initialize(url)
-    @url = url
-  end
+    def initialize(url)
+        @url = url
+    end
 
-  def scrape_and_save
-    page = URI.open(@url) 
-    doc = Nokogiri::HTML(page)
-    
-    title = doc.css('._1YokD2').css('h1').css('.B_NuCI').inner_text
-    description = doc.css('._1YokD2').css('._1AtVbE').css('._2o-xpa').css('._1mXcCf').css('p').inner_text
-    price = doc.css('._1YokD2').css('.dyC4hf').css('._30jeq3').inner_text
-    year = doc.css('._1YokD2').css('._1AtVbE').css('._1q8vHb').css('#swatch-0-model_year').css('a').inner_text
-    size = doc.css('._1YokD2').css('._1AtVbE').css('._1q8vHb').css('#swatch-0-display_size').css('a').inner_text
-    images = doc.css('._1YokD2').css('._1AtVbE').css('._1BweB8').css('img').attr('src').value
-    # Product.create_or_update!(
-        #   url: @url,
-        #   title: title,
-        #   description: description,
-        #   # ... other attributes ...
-        # )
-  end
+    def scrape_and_save
+        begin
+        page = URI.open(@url) 
+        doc = Nokogiri::HTML(page)
+        title = doc.css('._1YokD2').css('h1').css('.B_NuCI')&.text
+        category_name = title.split(' ').first
+        description = doc.css('._1mXcCf p')&.text
+        price = doc.css('._1YokD2').css('.dyC4hf').css('._30jeq3')&.text
+        year = doc.css('#swatch-0-model_year a')&.text
+        size = doc.css('#swatch-0-display_size a')&.text
+        image = doc.css('._1YokD2').css('._1AtVbE').css('._1BweB8').css('img').attr('src').value
+
+        category = Category.find_or_create_by(name: category_name)
+
+        product = Product.create!(
+            url: @url,
+            title: title,
+            description: description,
+            price: price,
+            model_year: year,
+            size: size,
+            category_id: category.id,
+        )
+        
+        product.product_images.create!(image: image) if image
+        rescue StandardError => e
+        puts "An error occurred while scraping: #{e.message}"
+        end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_11_122532) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_11_145518) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,7 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_11_122532) do
     t.string "url"
     t.string "title"
     t.text "description"
-    t.float "price"
+    t.string "price"
     t.string "model_year"
     t.string "size"
     t.bigint "category_id", null: false
