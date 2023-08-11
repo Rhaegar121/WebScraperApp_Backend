0753d1ca892dc875ffbbdb2845b3a023086d5638
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-08-11 15:49:49 +0630

    generate model for product, category, and productImage

---
+++ db/migrate/20230811091743_create_products.rb
@@ -0,0 +1,15 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :url
+      t.string :title
+      t.text :description
+      t.float :price
+      t.string :mobile_number
+      t.string :size
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
