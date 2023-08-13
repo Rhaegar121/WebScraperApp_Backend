0dec1c67fad68fdfd33adb301c9684885c339f68
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-08-13 20:30:30 +0630

    bypass cors policy with cors gem by giving temporary access

--- Gemfile
+++ Gemfile
@@ -72,3 +72,4 @@ group :test do
 end
 
 # gem 'sidekiq-cron'
+gem 'rack-cors'
--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module WebScraperApp
 
     # config.active_job.queue_adapter = :sidekiq
 
+    
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
